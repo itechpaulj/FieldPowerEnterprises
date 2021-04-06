350f5cdaffca35ef1b883ccadd52ba2d92b45a29
Paul Javinez <itechpaulj@gmail.com>
2021-04-06 19:56:01 +0800

    FirstCommit

---
+++ src/fieldpowerenterprises/database.java
@@ -0,0 +1,43 @@
+package fieldpowerenterprises;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Javinez
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class database {
+
+    
+    public static final Connection getConnection() {
+        Connection con = null;
+        
+        
+        // constant variable or unique variable
+        final String driver = "com.mysql.cj.jdbc.Driver";
+        final String host = "jdbc:mysql://localhost:";
+        final String port = "3306/";
+        final String db = "fieldpowerenterprises";
+        final String userRoot = "root";
+        final String passwordRoot = "";
+        final String concatAddresUrl= host+port+db;
+        
+        try{
+        Class.forName(driver);
+        con=(Connection)DriverManager.getConnection(concatAddresUrl,userRoot,passwordRoot);
+        
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        return con;
+        
+    }
+    
+}
---
+++ src/fieldpowerenterprises/login.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fieldpowerenterprises;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Javinez
+ */
+public class login {
+    
+    //Connection con;
+    PreparedStatement pst;
+    ResultSet rs;
+    
+    /* process login */
+//    public login() throws ClassNotFoundException, SQLException{
+//        String driver = "com.mysql.cj.jdbc.Driver";
+//        String server = "jdbc:mysql://localhost:";
+//        String port = "3306/";
+//        String db = "fieldpowerenterprises";
+//        String user = "root";
+//        String pass = "";
+//        String concat = server+port+db;
+//        
+//        
+//        try{
+//            Class.forName(driver);
+//            con = (Connection)DriverManager.getConnection(concat,user,pass);   
+//        }
+//        catch(SQLException | ClassNotFoundException e){
+//            e.printStackTrace();
+//        }
+//    }
+    /* process login */
+    
+    public boolean access_levelLogin(String email, String password) throws SQLException{
+        
+        try{
+            String sqlaccesslevel = "Select `email`, `password`, `access_level` from `userstbl` where `email`='"+email+"' AND `password`='"+password+"' ";
+            pst= (PreparedStatement)database.getConnection().prepareStatement(sqlaccesslevel);
+            rs=pst.executeQuery();
+
+            if(rs.next()==true){
+               String access = rs.getString("access_level");
+
+               if(access.equals("admin")){
+
+               JOptionPane.showMessageDialog(null,"This a admin");   
+
+               }
+               if(access.equals("user")){
+
+                JOptionPane.showMessageDialog(null,"This a user");   
+
+               }
+                return true; 
+
+            }
+            else{
+                JOptionPane.showMessageDialog(null,"Error");
+                return false;
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return false;
+       
+        
+    }
+}
