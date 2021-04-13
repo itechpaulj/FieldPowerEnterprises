0d82c463ae486cbf696b939647f16f04f9a09749
cosmicraymond09 <cosmicraymond09@gmail.com>
2021-04-13 14:08:02 +0800

    All Arrond

--- src/FPE/SupplierList.java
+++ src/FPE/SupplierList.java
@@ -88,9 +88,9 @@ public class SupplierList extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        AddSupplier as = new AddSupplier();
-        as.setVisible(true);
-      
+//        AddSupplier as = new AddSupplier();
+//        as.setVisible(true);
+//      
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
